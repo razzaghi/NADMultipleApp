faa5f0f21ab9a32b4d9cd9e0d99649a791c714c0
Mohammadreza <razzaghi229@gmail.com>
2015-08-09 01:57:18 +0430

    Add First Version

---
+++ apps/nad/NadCache.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/NadKernel.php';
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class NadCache extends HttpCache
+{
+}
---
+++ apps/panel/PanelCache.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/PanelKernel.php';
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class PanelCache extends HttpCache
+{
+}
